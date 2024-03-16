8386ac6a5ea3f6ab120ba60c7b2463d95e53a038
gaslug <163663250+gaslug@users.noreply.github.com>
2024-03-16 16:40:28 +0100

    Add files via upload

---
+++ led.js
@@ -0,0 +1,15 @@
+
+function updateThingSpeak(value) {
+  const writeApiKey = 'PD2PF82ZK1ZKM5G9';
+  const url = `https://api.thingspeak.com/update?api_key=${writeApiKey}&field1=${value}`;
+
+  fetch(url, { method: 'GET' })
+    .then(response => response.text())
+    .then(data => {
+      console.log(`ThingSpeak update response: ${data}`);
+      // Tutaj możesz dodać logikę potwierdzenia, np. zmianę stanu przycisku
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
