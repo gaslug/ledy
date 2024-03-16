4b372d00629a2ca35c8dd6a91ec00e6e120276a2
gaslug <163663250+gaslug@users.noreply.github.com>
2024-03-16 17:48:47 +0100

    Update led.js

--- led.js
+++ led.js
@@ -1,5 +1,15 @@
 
+let canClick = true; // Zmienna kontrolująca, czy można wysłać żądanie
+
 function updateThingSpeak(value) {
+  if (!canClick) {
+    console.log("Proszę poczekać 6 sekund przed kolejnym kliknięciem.");
+    return; // Zakończ funkcję, jeśli kliknięcie jest zablokowane
+  }
+
+  canClick = false; // Blokuj kolejne kliknięcia
+  countdown(6); // Rozpocznij odliczanie
+
   const writeApiKey = 'PD2PF82ZK1ZKM5G9';
   const url = `https://api.thingspeak.com/update?api_key=${writeApiKey}&field1=${value}`;
 
@@ -8,9 +18,23 @@ function updateThingSpeak(value) {
     .then(data => {
       console.log(`stan przycisku: ${value}`);
       console.log(`ThingSpeak update response: ${data}`);
-      // Tutaj możesz dodać logikę potwierdzenia, np. zmianę stanu przycisku
     })
     .catch((error) => {
       console.error('Error:', error);
     });
+}
+
+function countdown(seconds) {
+  const countdownTimer = document.getElementById('countdownTimer');
+  countdownTimer.innerHTML = `Poczekaj ${seconds} sekund(y) przed kolejnym kliknięciem.`;
+
+  let intervalId = setInterval(() => {
+    seconds--;
+    countdownTimer.innerHTML = `Poczekaj ${seconds} sekund(y) przed kolejnym kliknięciem.`;
+    if (seconds <= 0) {
+      clearInterval(intervalId);
+      countdownTimer.innerHTML = ""; // Usuń tekst licznika po zakończeniu odliczania
+      canClick = true;
+    }
+  }, 1000); // Odliczaj co 1 sekundę
 }
